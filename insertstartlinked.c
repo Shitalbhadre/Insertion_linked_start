a2c1c0c0eaf14ca01eeb7dc5f4b6e2517341f02c
Shitalbhadre <2023bit008@sggs.ac.in>
2024-08-25 20:54:03 +0530

    Add files via upload

---
+++ insertstartlinked.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+      int data;
+      struct Node *next;
+};
+
+void linkedListTraversal(struct Node* ptr){
+    while(ptr!=NULL){
+    printf("%d\n", ptr->data);
+    ptr = ptr->next;
+    }
+}
+
+struct Node * insertAtFirst(struct Node *head, int data){
+    struct Node * ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+    ptr->next = head;
+    return ptr;
+}
+
+int main(){
+       struct Node *head;
+       struct Node *second;
+       struct Node *third;
+
+       head = (struct Node *)malloc(sizeof(struct Node));
+       second = (struct Node *)malloc(sizeof(struct Node));
+       third = (struct Node *)malloc(sizeof(struct Node));
+
+        head->data = 7;
+        head->next = second;
+
+        second->data = 10;
+        second->next = third;
+
+        third->data = 13;
+        third->next = NULL;
+        
+        printf("Linked list before insertion\n");
+        linkedListTraversal(head);
+
+        printf("Linked list after insertion at start\n");
+        head = insertAtFirst(head,56);
+        linkedListTraversal(head);
+
+        return 0;
+}
